61378ce0881a80e6e69e92a2bea28378120f29dc
Seigm <selimontero02@gmail.com>
2023-02-28 11:21:28 -0500

    remove sudo permissions

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y python3
 mkdir ~/.deploy
 echo "alias env_generator='python3 ~/.deploy/env_generator/index.py'" >> ~/.bashrc
-sudo cp -r ~/env_generator ~/.deploy
+cp -r ~/env_generator ~/.deploy
 cd ~
 source .bashrc
 sudo rm -rf ~/env_generator
